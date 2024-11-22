032512551e728b3858c734d014fceca2261422b7
kumudharam <kumudaramanathan@gmail.com>
2024-11-22 19:24:41 +0530

    Dockerfile Changes

--- APIDevelopment/model_app.py
+++ APIDevelopment/model_app.py
@@ -1,9 +1,12 @@
 # importing libraires
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from sklearn.compose import _column_transformer
+_column_transformer._RemainderColsList = object
 
 import pandas as pd
 import joblib
+
 
 app = FastAPI()
 
@@ -17,8 +20,10 @@ class Input(BaseModel):
     no_of_trainings: int
     age: int
     length_of_service: int
-    KPIs_met: int
-    awards_won: int
+    #KPIs_met: int
+    #awards_won: int
+    awards_won: int = Field(..., alias="awards_won?")
+    KPIs_met: int = Field(..., alias="KPIs_met >80%")
     avg_training_score: int
 
 
@@ -26,17 +31,17 @@ class Output(BaseModel):
     is_promoted: int
 
 @app.post("/predict")
-def predict(data: Input) -> Output:
+async def predict(data: Input) -> Output:
     X_input = pd.DataFrame([[data.department,data.region,data.education,data.gender,
               data.recruitment_channel,data.no_of_trainings,data.age,data.previous_year_rating,
               data.length_of_service,data.KPIs_met,data.awards_won,data.avg_training_score]])
 
     X_input.columns = ['department','region','education','gender','recruitment_channel',
-                       'no_of_trainings','age','previous_year_rating','KPIs_met','awards_won',
+                       'no_of_trainings','age','previous_year_rating','length_of_service','KPIs_met >80%','awards_won?',
                        'avg_training_score']
 
     #load model
-    model = joblib.load('APIDevelopment/new_model_pipeline3.pkl')
+    model = joblib.load('APIDevelopment/model_pipeline.pkl')
 
     #predict
     prediction = model.predict(X_input)
